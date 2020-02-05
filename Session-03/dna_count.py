b8ddaae49dc9dd5572f28e25c423d1391c9d24e1
suke22 <s.oufrid.2019@alumnos.urjc.es>
2020-02-05 18:26:41 +0100

    Prueba....

---
+++ Session-03/dna_count.py
@@ -0,0 +1,21 @@
+dna = input("Introduce the sequence:" )
+
+counta = 0
+countc = 0
+countt = 0
+countg = 0
+
+for character in dna:
+    if character == "A":
+        counta += 1
+    elif character == "C":
+        countc += 1
+    elif character == "T":
+        countt += 1
+    elif character == "G":
+        countg += 1
+
+print("A: ", counta)
+print("C: ", countc)
+print("G: ", countg)
+print("T: ", countt)
