5bdfb888e9e2aa520d3b1a2ab5393c7c54778127
suke22 <s.oufrid.2019@alumnos.urjc.es>
2020-02-18 16:17:35 +0100

    Prueba....

---
+++ Session-06/test-color.py
@@ -0,0 +1,6 @@
+import termcolor
+
+termcolor.cprint("Testing the colors", "green")
+termcolor.cprint("Anoter color", "blue")
+termcolor.cprint("Anoter color", "red")
+termcolor.cprint("Anoter color", "yellow")
