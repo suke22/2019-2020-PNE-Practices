050284cd01719b7616921dee9933f29e3aadaeb8
suke22 <s.oufrid.2019@alumnos.urjc.es>
2020-02-18 16:47:02 +0100

    Prueba....

--- P0/Ex3.py
+++ P0/Ex3.py
@@ -5,6 +5,7 @@ FILENAME1 = "U5.txt"
 FILENAME2 = "ADA.txt"
 FILENAME3 = "FRAT1.txt"
 FILENAME4 = "FXN.txt"
+list = ["U5.txt", "ADA.txt", "FRAT1.txt", "FXN.txt"]
 
 print("Gene U5 ---> Length:", seq_len(FOLDER+FILENAME1))
 print("Gene ADA ---> Length:", seq_len(FOLDER+FILENAME2))
---
+++ Session-06/Ex1.py
@@ -0,0 +1,4 @@
+s1 = Seq("ACCTGC")
+s2 = Seq("Hello? Am I a valid sequence?")
+print(f"Sequence 1: {s1}")
+print(f"Sequence 2: {s2}")
