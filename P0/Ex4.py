0b43708e91d01b60f357bb18d93fcf0c8d6ffcf5
suke22 <s.oufrid.2019@alumnos.urjc.es>
2020-02-19 18:23:40 +0100

    Prueba....

--- P0/Ex4.py
+++ P0/Ex4.py
@@ -1,10 +1,10 @@
 from Seq0 import *
 
 FOLDER = "../Session-04/"
-list = ["U5.txt", "ADA.txt", "FRAT1.txt", "FXN.txt"]
+list = ["U5", "ADA", "FRAT1", "FXN"]
 bases = ["A", "C", "G", "T"]
 for text in list:
-    print(text, ":")
+    print("Gene", text, ":")
     for element in bases:
-        print("", element, ":", seq_count_base(seq_read_fasta(FOLDER+text), element))
+        print("", element, ":", seq_count_base(seq_read_fasta(FOLDER+text+".txt"), element))
     print("\n")
