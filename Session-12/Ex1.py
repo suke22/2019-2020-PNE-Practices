ab703eb9c14fa25be5e4adaa40704cc644209645
suke22 <s.oufrid.2019@alumnos.urjc.es>
2020-04-24 15:57:05 +0200

    session 12

---
+++ Session-12/Ex1.py
@@ -0,0 +1,10 @@
+#Which is the request line?  GET /hello HTTP/1.1
+#Which is the resource name that the client is asking for? /hello
+
+#Which is the request line?  GET /file.html HTTP/1.1
+#Which is the resource name that the client is asking for? /file.html
+
+#Which is the request line?  GET /hi/there?name=virus&type=corona HTTP/1.1
+#Which is the resource name that the client is asking for? /hi/there?name=virus&type=corona
+
+#What should be the URL that we have to write in the Browser for accessing the /dna/u5 resource? GET /dna/u5 HTTP/1.1
