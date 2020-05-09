b443df7ec539baa9eff0758a4a815c154782eafa
suke22 <s.oufrid.2019@alumnos.urjc.es>
2020-05-09 15:27:09 +0200

    session 18

---
+++ Session-18/test-json-EX01.py
@@ -0,0 +1,39 @@
+import json
+import termcolor
+from pathlib import Path
+
+# -- Read the json file
+jsonstring = Path("people-EX01.json").read_text()
+
+# Create the object person from the json string
+persons = json.loads(jsonstring)
+
+# Person is now a dictionary. We can read the values
+# associated to the fields 'Firstname', 'Lastname' and 'age'
+
+# Print the information on the console, in colors
+print(f"Total people in the database: {len(persons)}")
+
+for person in persons:
+    print()
+    termcolor.cprint("Name: ", 'green', end="")
+    print(person['Firstname'], person['Lastname'])
+    termcolor.cprint("Age: ", 'green', end="")
+    print(person['age'])
+
+    # Get the phoneNumber list
+    phoneNumbers = person['phoneNumber']
+
+    # Print the number of elements in the list
+    termcolor.cprint("Phone numbers: ", 'green', end='')
+    print(len(phoneNumbers))
+
+    # Print all the numbers
+    for i, num in enumerate(phoneNumbers):
+        termcolor.cprint("  Phone {}:".format(i), 'blue')
+
+        # The element num contains 2 fields: number and type
+        termcolor.cprint("    Type: ", 'red', end='')
+        print(num['type'])
+        termcolor.cprint("    Number: ", 'red', end='')
+        print(num['number'])
