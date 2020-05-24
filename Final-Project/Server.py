4647df0776bf0e631858bbbd7b6b02553d4caf35
suke22 <s.oufrid.2019@alumnos.urjc.es>
2020-05-24 20:28:13 +0200

    Final Project

--- Final-Project/Server.py
+++ Final-Project/Server.py
@@ -13,6 +13,7 @@ bases = ["A", "T", "C", "G"]
 socketserver.TCPServer.allow_reuse_address = True
 
 
+# we need a function to compute de response html
 def document(title, color):
     return f"""
             <!DOCTYPE html>
@@ -29,6 +30,7 @@ def document(title, color):
             """
 
 
+# we need a function to connect with ensembl data
 def info_json(endpoint):
     port = 8080
     server = 'rest.ensembl.org'
@@ -64,27 +66,27 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         elif verb == "/listSpecies":
             limit = req_line.split("=")[1]
             try:
-                info = info_json("info/species?")["species"]
-                contents = document("LIST OF SPECIES IN THE BROWSER", "lightblue")
+                info = info_json("info/species?")["species"]  # here we get the all the species
+                contents = document("LIST OF SPECIES IN THE BROWSER", "plum")  # we compute the html response
                 contents += f"""<h4>The total number of species in ensembl is: 286</h4><p>"""
                 contents += f"""<h4>The limit you have selected is: {limit}</h4><p>"""
-                if limit == "":
+                if limit == "":  # if the client does not enter anything
                     contents += f"""<h4>The names of the species are:</h4>"""
                     for element in info:
-                        contents += f"""<p> • {element["display_name"]}</p>"""
+                        contents += f"""<p> • {element["display_name"]}</p>"""  # get the species name
                     self.send_response(200)
-                elif 286 >= int(limit) >= 0:
+                elif 286 >= int(limit) >= 0:  # if the client enter a valid limit
                     counter = 0
                     contents += f"""<h4>The names of the species are:</h4>"""
                     for element in info:
-                        if counter < int(limit):
-                            contents += f"""<p> • {element["display_name"]}</p>"""
+                        if counter < int(limit):  # get the correct number of species for the limit
+                            contents += f"""<p> • {element["display_name"]}</p>"""  # we just ger the specie's name
                             counter += 1
                     self.send_response(200)
-                else:
+                else:  # if the client enter a value out of range
                     contents = Path('Error.html').read_text()
                     self.send_response(404)
-            except ValueError:
+            except ValueError:  # if the client enter a word
                 contents = Path('Error.html').read_text()
                 self.send_response(404)
             contents += f"""<a href="/">Main page</a></body></html>"""
@@ -92,17 +94,18 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         # 2) Information about the karyotype
         elif verb == "/karyotype":
             specie = req_line.split("=")[1]
-            list_species = info_json("info/species?")["species"]
+            list_species = info_json("info/species?")["species"]  # we get all the species
+            contents = ""
             for dictionary in list_species:
-                if specie in dictionary.values():
-                    info = info_json("info/assembly/" + specie + "?")["karyotype"]
-                    contents = document("KARYOTYPE OF A SPECIFIC SPECIES", "lightblue")
+                if specie in dictionary.values():  # check if the specie entered by the client is valid
+                    info = info_json("info/assembly/" + specie + "?")["karyotype"]  # get the karyotype of the species
+                    contents = document("KARYOTYPE OF A SPECIFIC SPECIES", "plum")  # we compute the html response
                     contents += f"""<h4> The names of the {specie}'s chromosomes are: </h4>"""
                     for element in info:
                         contents += f"""<p> • {element}</p>"""
                     self.send_response(200)
                     break
-                else:
+                else:  # if the species does not exist
                     contents = Path('Error.html').read_text()
                     self.send_response(404)
             contents += f"""<a href="/">Main page</a></body></html>"""
@@ -112,66 +115,69 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             number = req_line.split("=")[2]
             values = req_line.split("=")[1]
             specie = values.split("&")[0]
-            list_species = info_json("info/species?")["species"]
+            list_species = info_json("info/species?")["species"]  # we get all the species
+            contents = ""
             for dictionary in list_species:
-                if specie in dictionary.values():
-                    info = info_json("info/assembly/" + specie + "?")["top_level_region"]
+                if specie in dictionary.values():  # check if the specie entered by the client is valid
+                    info = info_json("info/assembly/" + specie + "?")["top_level_region"]  # get all the lengths
                     for element in info:
-                        if element["name"] == number:
-                            contents = document("LENGTH OF A SPECIFIC CHROMOSOME", "lightblue")
+                        if element["name"] == number:  # check if the chromosome is valid
+                            # we compute the html response
+                            contents = document("LENGTH OF A SPECIFIC CHROMOSOME", "plum")
                             contents += f"""<h4> The length of the chromosomes {number} is: </h4>"""
                             contents += f"""<p> {element["length"]} </p>"""
                             self.send_response(200)
                             break
-                        else:
+                        else:  # if the chromosome does not exist
                             contents = Path('Error.html').read_text()
                             self.send_response(404)
                     break
-                else:
+                else:  # if the chromosome does not exist
                     contents = Path('Error.html').read_text()
                     self.send_response(404)
             contents += f"""<a href="/">Main page</a></body></html>"""
 
-        #4) Sequence of a human gene
+        # 4) Sequence of a human gene
         elif verb == "/geneSeq":
             value = req_line.split("=")[1]
-            if value == "":
+            if value == "":  # if the client does not enter anything
                 contents = Path('Error.html').read_text()
                 self.send_response(404)
             else:
-                try:
+                try:  # if the value is a number it prints an error
                     value = int(value)
                     contents = Path('Error.html').read_text()
                     self.send_response(404)
                 except ValueError:
-                    try:
-                        gene_id = info_json(f"/xrefs/symbol/homo_sapiens/{value}?")[0]["id"]
-                        seq = info_json(f"/sequence/id/{gene_id}?")
-                        contents = document("GENE SEQUENCE", "lightblue")
+                    try:  # if the value is not a number
+                        gene_id = info_json(f"/xrefs/symbol/homo_sapiens/{value}?")[0]["id"]  # get the id
+                        seq = info_json(f"/sequence/id/{gene_id}?")   # get all the information about the gene
+                        contents = document("GENE SEQUENCE", "plum")  # we compute the html response
                         contents += f'<p> The sequence of gene {value} is: </p>'
+                        # get the sequence of the id
                         contents += f'<textarea rows = "30" cols = "150"> {seq["seq"]} </textarea>'
                         self.send_response(200)
-                    except IndexError:
+                    except IndexError:  # if the gene does not exist
                         contents = Path('Error.html').read_text()
                         self.send_response(404)
             contents += f"""<a href="/">Main page</a></body></html>"""
 
-        #5) Information about a human gene
+        # 5) Information about a human gene
         elif verb == "/geneInfo":
             value = req_line.split("=")[1]
-            if value == "":
+            if value == "":  # if the client does not enter anything
                 contents = Path('Error.html').read_text()
                 self.send_response(404)
             else:
-                try:
+                try:  # if the value is not a number
                     value = int(value)
                     contents = Path('Error.html').read_text()
                     self.send_response(404)
                 except ValueError:
                     try:
-                        gene_id = info_json(f"/xrefs/symbol/homo_sapiens/{value}?")[0]["id"]
-                        seq_info = info_json(f"/lookup/id/{gene_id}?")
-                        contents = document("SEQUENCE INFORMATION", "lightblue")
+                        gene_id = info_json(f"/xrefs/symbol/homo_sapiens/{value}?")[0]["id"]  # get the id
+                        seq_info = info_json(f"/lookup/id/{gene_id}?")  # get all the information about the gene
+                        contents = document("SEQUENCE INFORMATION", "plum")  # we compute the html response
                         contents += f'<h4> The sequence of gene {value} is: </h4>'
                         contents += f'<p> The start point is: {seq_info["start"]} </p>'
                         contents += f'<p> The end point is: {seq_info["end"]} </p>'
@@ -179,74 +185,75 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                         contents += f'<p> The id is: {seq_info["id"]} </p>'
                         contents += f'<p> The chromose is: {seq_info["seq_region_name"]} </p>'
                         self.send_response(200)
-                    except IndexError:
+                    except IndexError:  # if the gene does not exist
                         contents = Path('Error.html').read_text()
                         self.send_response(404)
             contents += f"""<a href="/">Main page</a></body></html>"""
 
-        #6) Total length and % of bases os a human gene
+        # 6) Total length and % of bases os a human gene
         elif verb == "/geneCalc":
             value = req_line.split("=")[1]
-            if value == "":
+            if value == "":   # if the client does not enter anything
                 contents = Path('Error.html').read_text()
                 self.send_response(404)
             else:
-                try:
+                try:  # if the value is not a number
                     value = int(value)
                     contents = Path('Error.html').read_text()
                     self.send_response(404)
                 except ValueError:
                     try:
-                        gene_id = info_json(f"/xrefs/symbol/homo_sapiens/{value}?")[0]["id"]
-                        sequence = info_json(f"/sequence/id/{gene_id}?")["seq"]
+                        gene_id = info_json(f"/xrefs/symbol/homo_sapiens/{value}?")[0]["id"]  # get the id
+                        sequence = info_json(f"/sequence/id/{gene_id}?")["seq"]  # get the sequence of the gene
                         seq = Seq(sequence)
-                        contents = document("SEQUENCE INFORMATION", "lightblue")
+                        contents = document("SEQUENCE CALCULATIONS", "plum")  # we compute the html response
                         contents += f'<h4> Calculations over the introduced gene {value}: </h4>'
-                        contents += f'<p> Total length of this gene is: {seq.len()}</p>'
+                        contents += f'<p> Total length of this gene is: {seq.len()}</p>'  # calculate the length
                         contents += f'<p> Percentage of the bases is: </p>'
-                        for base in bases:
+                        for base in bases:  # calculate the count and the percentage od the sequence
                             count = seq.count_base(base)
                             percentage = round(seq.count_base(base) * (100 / seq.len()), 2)
                             contents += f'<p> {base}: {count} ({percentage}%) </p>'
                         self.send_response(200)
-                    except IndexError:
+                    except IndexError:  # if the gene does not exist
                         contents = Path('Error.html').read_text()
                         self.send_response(404)
             contents += f"""<a href="/">Main page</a></body></html>"""
 
-        #7) List of genes located in a chromosome
+        # 7) List of genes located in a chromosome
         elif verb == "/geneList":
             value = req_line.split("?")[1]
             chromo_number, start_number, end_number = value.split("&")
             chromo = chromo_number.split("=")[1]
             start = start_number.split("=")[1]
             end = end_number.split("=")[1]
-            if chromo == "" or start == "" or end == "":
+            if chromo == "" or start == "" or end == "":  # if the client does not enter anything
                 contents = Path('Error.html').read_text()
                 self.send_response(404)
             else:
                 try:
-                    start = int(start)
-                    end = int(end)
-                    if chromo in ["x", "X", "y", "Y" , "MT"] or 1 <= int(chromo) <= 22 :
+                    start = int(start)  # check if the start point is a number
+                    end = int(end)  # check if the start point is a number
+                    # check if the chromosome is human
+                    if chromo in ["x", "X", "y", "Y", "MT"] or 1 <= int(chromo) <= 22:
+                        # get the genes inside that positions
                         genes = info_json(f"/overlap/region/human/{chromo}:{start}-{end}?feature=gene;")
                         try:
-                            contents = document("GENE LIST", "lightblue")
+                            contents = document("GENE LIST", "plum")  # we compute the html response
                             contents += f"<h4>Genes located in the chromosome {chromo} from {start} to {end} positions </h4>"
                             for list in genes:
                                 contents += f'<p> - {list["external_name"]}</p>'
                             self.send_response(200)
-                        except TypeError:
+                        except TypeError:  # if the start or end points are no valid
                             contents = Path('Error.html').read_text()
                             self.send_response(404)
-                    else:
+                    else:  # if the chromosome is not one of the human
                         contents = Path('Error.html').read_text()
                         self.send_response(404)
-                except ValueError:
+                except ValueError:  # if something entered by the client is not a number
                     contents = Path('Error.html').read_text()
                     self.send_response(404)
             contents += f"""<a href="/">Main page</a></body></html>"""
-
 
         else:
             contents = Path('Error.html').read_text()
